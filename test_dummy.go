a81ace9ccc7165403cc1acfd1725ec185db82f4f
Ed Zynda <ezynda3@gmail.com>
2026-01-24 02:17:50 +0300

    Edit test_dummy.go: add greet function and update main

--- test_dummy.go
+++ test_dummy.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+func greet(name string) string {
+	return fmt.Sprintf("Hello, %s!", name)
+}
+
 func main() {
 	fmt.Println("This is a dummy test file")
+	message := greet("iteratr")
+	fmt.Println(message)
 }
